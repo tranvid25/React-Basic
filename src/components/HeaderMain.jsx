a6e36e71d6151753ab38453142d18c11b113c7c5
tranvid25 <lsdnh041004@gmail.com>
2025-07-02 22:59:27 +0700

    Authcontext

--- src/components/HeaderMain.jsx
+++ src/components/HeaderMain.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { useAuth } from "../contexts/auth-context";
+import { set } from "react-hook-form";
 const HeaderMain = () => {
-  const {user}=useAuth();
+  const { user, setUser } = useAuth();
   return (
     <div className="flex items-center justify-around p-4 bg-white shadow-md">
       <div className="flex items-center gap-x-3">
         <img
-          src="https://images.unsplash.com/photo-1748392029321-58793571f850?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
-          alt=""
+          src={user.avatar}
           className="object-cover w-10 h-10 rounded-full"
         />
-        <span className="text-sm font-medium">welcomback 
+        <span className="text-sm font-medium">
+          welcomback
           <strong>{user.fullname}</strong>
         </span>
       </div>
-      <span className="text-sm font-medium">{user.email}</span>
+      <button className="p-2 bg-gray-400 rounded-lg" onClick={() => setUser(null)}>
+        Sign out
+      </button>
     </div>
   );
 };
--- src/contexts/auth-context.jsx
+++ src/contexts/auth-context.jsx
@@ -8,7 +8,7 @@ function AuthProvider(props) {
     fullname: "Trần Vĩ",
     work: "Developer",
     email: "vit76404@gmail.com",
-    age: 20,
+    avatar:"https://images.unsplash.com/photo-1750544684965-873a86eba4b6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
   });
 
   const value = { user, setUser }; // nên dùng object
